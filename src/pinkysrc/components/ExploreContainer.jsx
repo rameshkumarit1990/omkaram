44b90ff830033edf6a14b66d134466366f3a2394
sravanilakkoju <67959721+sravanilakkoju@users.noreply.github.com>
2024-05-04 20:15:15 +0530

    Added devotional files
    Added devotional files

---
+++ src/pinkysrc/components/ExploreContainer.jsx
@@ -0,0 +1,12 @@
+import './ExploreContainer.css';
+
+const ExploreContainer = (props) => {
+  const { name } = props
+  return (
+    <div id="container">
+
+    </div>
+  );
+};
+
+export default ExploreContainer;
